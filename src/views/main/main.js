67858e8d829379f7ae77218db652c41aa9551c2c
Vitaliy Semenov <synthex@bk.ru>
2023-06-30 17:12:54 +0600

    feat: add reactivity

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,15 +1,32 @@
 import { View } from "../../common/view";
+import onChange from 'on-change';
 
 export class MainView extends View {
-  constructor() {
+  state = {
+    list: [],
+    loading: false,
+    searchQuery: undefined,
+    offset: 0
+  }
+
+  constructor(appState) {
     super();
+    this.appState = appState;
+    this.appState = onChange(this.appState, this.appStateHook.bind(this));
     this.setTitle('Book Search');
+  }
+
+  appStateHook(path) {
+    if (path === 'favorites') {
+      console.log(path);
+    }
   }
 
   render() {
     const main = document.createElement('div');
-    main.innerHTML = 'Test';
+    main.innerHTML = `Books amount: ${this.appState.favorites.length}`;
     this.app.innerHTML = ''
     this.app.append(main);
+    this.appState.favorites.push('123');
   }
 }
